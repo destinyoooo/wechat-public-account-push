2fe881c866cc0b0ae398b94352068b198764d765
destinyoooo <57470814+destinyoooo@users.noreply.github.com>
2024-12-03 18:49:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,1 +1,49 @@
-{"APP_ID":"wxa071bd9ee51e7412","APP_SECRET":"22f5bb6b07243cb3b7ccce04d6d5aa64","IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"","CALLBACK_USERS":[{"name":"自己","id":""}],"USERS":[{"name":"Destiny","id":"opo6w6NNhmTNJYhqBP1kzkpbw0DQ","useTemplateId":"_51Mef5XGOkTKw6zu8ePA83hL2lXimQk3qlDiqKNWPY","province":"四川","city":"成都","horoscopeDate":"10-01","horoscopeDateType":"今日","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"生日","name":"生日提醒","date":"10-01","year":"2024"}],"customizedDateList":[{"date":"NaN-NaN-NaN"}]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":true,"earthyLoveWords":true,"momentCopyrighting":true,"poisonChickenSoup":true,"poetry":true,"horoscope":true,"birthdayMessage":true}}
+{
+    "APP_ID": "wxa071bd9ee51e7412",
+    "APP_SECRET": "22f5bb6b07243cb3b7ccce04d6d5aa64",
+    "IS_SHOW_COLOR": true,
+    "CALLBACK_TEMPLATE_ID": "",
+    "CALLBACK_USERS": [
+        {
+            "name": "自己",
+            "id": ""
+        }
+    ],
+    "USERS": [
+        {
+            "name": "Destiny",
+            "id": "opo6w6NNhmTNJYhqBP1kzkpbw0DQ",
+            "useTemplateId": "_51Mef5XGOkTKw6zu8ePA83hL2lXimQk3qlDiqKNWPY",
+            "province": "四川",
+            "city": "成都",
+            "horoscopeDate": "10-01",
+            "horoscopeDateType": "今日",
+            "openUrl": "https://shuangxunian.github.io/",
+            "festivals": [
+                {
+                    "type": "生日",
+                    "name": "生日提醒",
+                    "date": "10-01",
+                    "year": "2024"
+                }
+            ],
+            "customizedDateList": [
+                {
+                    "date": "NaN-NaN-NaN"
+                }
+            ]
+        }
+    ],
+    "SWITCH": {
+        "weather": true,
+        "holidaytts": true,
+        "CIBA": true,
+        "oneTalk": true,
+        "earthyLoveWords": true,
+        "momentCopyrighting": true,
+        "poisonChickenSoup": true,
+        "poetry": true,
+        "horoscope": true,
+        "birthdayMessage": true
+    }
+}
